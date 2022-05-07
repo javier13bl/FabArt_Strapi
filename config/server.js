cc9e3765b3a4fd8944ea6c11107c7cc553af077b
javier13bl <javier13bl@gmail.com>
2022-05-07 10:56:28 -0500

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '558879bad0dd44c4f4625e3585d25881'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '802c75c0-8a99-4ec4-aba2-fe452afbabd1'
+};
